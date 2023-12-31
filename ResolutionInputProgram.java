d44bdf6db27ed8a7c44a654a5475c7cdd011e23b
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-01-01 01:53:34 +0530

    Writing down your Resolutions into a File

--- ResolutionInputProgram.java
+++ ResolutionInputProgram.java
@@ -9,36 +9,41 @@ public class ResolutionInputProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> resolutions = new ArrayList<>();
+        try {
+            ArrayList<String> resolutions = new ArrayList<>();
 
-        // Input resolutions
-        System.out.println("Resolution Input Program");
-        while (true) {
-            System.out.print("Enter a resolution (or type 'exit' to finish): ");
-            String resolution = scanner.nextLine();
+            // Input resolutions
+            System.out.println("New Year Resolution - Input Program");
+            while (true) {
+                System.out.print("Enter a resolution ( or type 'exit' to finish): ");
+                String resolution = scanner.nextLine() + " - Incomplete";
 
-            if (resolution.equalsIgnoreCase("exit")) {
-                break;
+                if (resolution.equalsIgnoreCase("exit - Incomplete")) {
+                    break;
+                }
+
+                resolutions.add(resolution);
             }
 
-            resolutions.add(resolution);
+            // Save resolutions to file
+            saveResolutions(resolutions);
+
+            System.out.println("Resolutions saved successfully. You can now use the Resolution Editor Program.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            scanner.close();
         }
-
-        // Save resolutions to file
-        saveResolutions(resolutions);
-
-        System.out.println("Resolutions saved successfully. You can now use the Resolution Editor Program.");
-        scanner.close();
+        
     }
 
-    private static void saveResolutions(ArrayList<String> resolutions) {
+    private static void saveResolutions(ArrayList<String> resolutions) throws IOException {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(RESOLUTIONS_FILE))) {
             for (String resolution : resolutions) {
                 bw.write(resolution);
                 bw.newLine();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
